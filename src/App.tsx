a40d1feddebc2700c918790a02e6df93596d439e
JolantaDubas <jooldu@gmail.com>
2022-03-30 23:47:20 +0200

    basic layout created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ChatContainer>
+        <h1>Chat application</h1>
+        <p>Switch user</p>
+        <button>Switch</button>
+        <ChatBoxContainer>ChatBox</ChatBoxContainer>
+      </ChatContainer>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  display: grid;
+  height: 100vh;
+  justify-content: center;
+`;
+const ChatContainer = styled.div`
+  display: grid;
+  height: 100%;
+  grid-template-rows: repeat(3, min-content) auto;
+  gap: 16px;
+  padding-top: 42px;
+  padding-bottom: 12px;
+  box-sizing: border-box;
+
+  h1,
+  p {
+    margin: 0;
+  }
+`;
+
+const ChatBoxContainer = styled.div`
+  display: grid;
+  gap: 14px;
+  height: 100%;
+  width: 500px;
+  border: 1px solid #70707033;
+  border-radius: 18px;
+`;
 export default App;
